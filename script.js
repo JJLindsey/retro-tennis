27cf847d28e29236917aaecd2ae82c75b4fb83b9
JJLindsey <jlindsey010@gmail.com>
2022-02-25 10:19:37 -0800

    pseudo code

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+//draw background - court
+
+//create ball for play
+
+//create paddles/bars for players
+
+//functions for ball to move from each side of screen
+
+//function to move paddles
+
 const canvas = document.getElementById('game-court');
 const context = canvas.getContext('2d');
-
+//use p5js??
 
 function drawRect (x,y,w,h,color) {
     context.fillStyle = "navy";
